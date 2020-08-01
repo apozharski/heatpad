d610a4a75d9bd74fe5f5074b66881ac7230839c9
Anton Pozharskiy <apozharski@gmail.com>
2020-08-01 13:26:09 -0400

    initial tests

--- HeatpadSketch/HeatpadSketch.ino
+++ HeatpadSketch/HeatpadSketch.ino
@@ -1,9 +1,25 @@
+#include <Arduino.h>
 #include <Wire.h>
 #include <LCD.hpp>
-#include <PID_v1.h>
+//#include <PID_v1.h>
+
+#define RED   2
+#define GREEN 3
+#define BLUE  5
+
+nhd::LCD<2,16> lcd;
 
 void setup() {
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  analogWrite(RED,255);
+  analogWrite(GREEN,0);
+  analogWrite(BLUE,0);
+  
+  lcd.init();
 }
 
 void loop() {
+  
 }
